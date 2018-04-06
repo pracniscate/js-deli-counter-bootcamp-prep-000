00c4b964dfaa9ee60ff7fc872d714ede4e704de4
Lorena <lorena.giddens@outlook.com>
2018-04-06 04:00:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var otherDeli = ["Steven", "Blake", "Avi"];
 
 // adds a person to the line
 
-function takeANumber(katzDeliLine, name) {
+function takeANumber(katzDeliLine, Ada) {
   var name = Ada;
-  katzDeli.push("Welcome, Ada. You are number 1 in line.");
+  katzDeli.push("Welcome, name. You are number 1 in line.");
   return katzDeli;
 }
 
