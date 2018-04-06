6226f8df329931a6eb21186e1a84538d9b20bbc5
Lorena <lorena.giddens@outlook.com>
2018-04-06 04:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,19 +20,15 @@ function nowServing(x) {
 }
 
 function currentLine(line) {
-  var x
-  
-}
-
-function currentLine(x) {
-    var line = []
-    if (x.length === 0) {
-      return "The line is currently empty."
-    } else {
-      for(var i = 0; i < x.length; i++) {
-        line += (i + 1) + ". " + x[i] + ", "
-      }
-      line = line.slice(0, line.length-2)
-      return "The line is currently: " + line
+  var x = [];
+  if (line.length === 0) {
+    return "The line is currently empty.";
+  } else {
+    for(var i = 0; i < line.length; i++) {
+      x += (i + 1) + ". " + line[1] + ", ";
     }
+  x = x.slice(0, x.length-2);
+  return "The line is currently: " + x;
+  }
+  
 }
