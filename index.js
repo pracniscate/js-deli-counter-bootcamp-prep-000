5f739ac42413dab8c4ea2e460cc58f3a33efb937
Lorena <lorena.giddens@outlook.com>
2018-04-06 04:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var katzDeli = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
-// adds a person to the line
+/* adds a person to the line
+appends the person the end of the line if there are already people on it
+properly handles multiple people being added */
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return("Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.");
 }
 
-// appends the person the end of the line if there are already people on it
 
 
