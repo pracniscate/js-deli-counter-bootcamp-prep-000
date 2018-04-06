f192bf19cdfafb6269071e0b1d58ed65ce9501a3
Lorena <lorena.giddens@outlook.com>
2018-04-06 03:57:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 var otherDeli = ["Steven", "Blake", "Avi"];
 
 function takeANumber(katzDeliLine, name) {
-  console.log("Welcome, . You are number 1 in line.");
+  console.log(`Welcome, ${name}. You are number 1 in line.`);
   return katzDeli;
 }
 
