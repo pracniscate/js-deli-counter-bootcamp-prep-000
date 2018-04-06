ad37001bbfc9c6adb1095bc124a63a973ddf1996
Lorena <lorena.giddens@outlook.com>
2018-04-06 04:45:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ function nowServing(x) {
   }
 }
 
-function currentLine(line) {
+
+
+/* function currentLine(line) {
   var x = [];
   if (line.length === 0) {
     return "The line is currently empty.";
@@ -30,4 +32,4 @@ function currentLine(line) {
   x = x.slice(0, x.length-2);
   return "The line is currently: " + x;
   }
-}
+} */
